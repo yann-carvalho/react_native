efc512c2bd68677e7cc442b7f1766bf2d445ae32
yann-carvalho <yanncarvalho95@gmail.com>
2025-02-19 23:01:02 -0300

    aula01

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Exercicio1 from './src/components/exercicio1'
+
+const App = () => {
+  return (
+    <View>
+      <Exercicio1 nome = "Yann" />
+    </View>
+  )
+}
+
+export default App
