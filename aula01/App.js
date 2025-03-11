01166d1da4025e87790d94c4067dba7fb04f6363
yann-carvalho <yanncarvalho95@gmail.com>
2025-03-11 07:58:26 -0300

    new

--- aula01/App.js
+++ aula01/App.js
@@ -2,11 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Exercicio1 from './src/components/Exercicio1'
 import Exercicio2 from './src/components/Exercicio2'
+import Exercicio3 from './src/components/Exercicio3'
+import Exercicio4 from './src/components/Exercicio4'
 
 const App = () => {
   return (
     <View>
-      <Exercicio2 />
+      <Exercicio4 />
     </View>
   )
 }
